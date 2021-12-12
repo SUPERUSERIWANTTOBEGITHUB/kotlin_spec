d36ff1cf41119217b14a2d285eb1e77f7e5ff37b
AS <yvg68@yandex.ru>
2021-12-12 18:53:02 +0300

    init

---
+++ app/src/main/java/yvg68/measurer/kotlin_spec/MainActivity.kt
@@ -0,0 +1,11 @@
+package yvg68.measurer.kotlin_spec
+//my first comment
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
