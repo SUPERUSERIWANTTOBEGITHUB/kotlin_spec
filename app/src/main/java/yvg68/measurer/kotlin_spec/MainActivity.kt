494e4a52cb56b5db589c4d2f61c776eb951bf8d6
AS <yvg68@yandex.ru>
2021-12-12 19:08:30 +0300

    comment onCreate(savedInstanceState setContentView(R.layout.activity_main)

--- app/src/main/java/yvg68/measurer/kotlin_spec/MainActivity.kt
+++ app/src/main/java/yvg68/measurer/kotlin_spec/MainActivity.kt
@@ -5,7 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState) //сохраняет тек состояние при повороте экране и при нехватке памяти
+        setContentView(R.layout.activity_main) // запускает файл activity_main.xml
     }
+
 }
